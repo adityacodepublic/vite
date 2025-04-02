2123de968ee0deff89cdddcc97077bf12d9e92df
gitcode <website0wordpress@gmail.com>
2025-04-03 00:07:59 +0530

    input

--- src/components/control-tray/ControlTray.tsx
+++ src/components/control-tray/ControlTray.tsx
@@ -1,10 +1,23 @@
 import cn from "classnames";
 import { memo, ReactNode, RefObject, useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import { useLiveAPIContext } from "@/contexts/LiveAPIContext";
 import { UseMediaStreamResult } from "@/hooks/use-media-stream-mux";
 import { useScreenCapture } from "@/hooks/use-screen-capture";
 import { useWebcam } from "@/hooks/use-webcam";
-import { ArrowUp, Square } from "lucide-react";
+import {
+  ArrowUp,
+  Ellipsis,
+  Globe,
+  Mic,
+  MicOff,
+  MonitorOff,
+  Plus,
+  ScreenShare,
+  Square,
+  Video,
+  VideoOff,
+} from "lucide-react";
 import { AudioRecorder } from "@/lib/live/audio-recorder";
 import AudioPulse from "../audio-pulse/AudioPulse";
 import "./control-tray.scss";
@@ -15,6 +28,8 @@ import {
   PromptInputAction,
 } from "../ui/prompt-input";
 import { Button } from "../ui/button";
+import { Arrow } from "@radix-ui/react-tooltip";
+import { Waveform } from "../ui/waveform";
 
 export type ControlTrayProps = {
   videoRef: RefObject<HTMLVideoElement>;
@@ -25,8 +40,9 @@ export type ControlTrayProps = {
 
 type MediaStreamButtonProps = {
   isStreaming: boolean;
-  onIcon: string;
-  offIcon: string;
+  onIcon: ReactNode;
+  offIcon: ReactNode;
+  disabled?: boolean;
   start: () => Promise<any>;
   stop: () => any;
 };
@@ -35,15 +51,30 @@ type MediaStreamButtonProps = {
  * button used for triggering webcam or screen-capture
  */
 const MediaStreamButton = memo(
-  ({ isStreaming, onIcon, offIcon, start, stop }: MediaStreamButtonProps) =>
+  ({
+    isStreaming,
+    onIcon,
+    offIcon,
+    start,
+    stop,
+    disabled,
+  }: MediaStreamButtonProps) =>
     isStreaming ? (
-      <button className="action-button" onClick={stop}>
-        <span className="material-symbols-outlined">{onIcon}</span>
-      </button>
+      <Button
+        className=" bg-background text-secondary-foreground hover:bg-secondary h-11 w-11 rounded-2xl font-semibold focus-visible:outline-black"
+        onClick={stop}
+        disabled={disabled}
+      >
+        {onIcon}
+      </Button>
     ) : (
-      <button className="action-button" onClick={start}>
-        <span className="material-symbols-outlined">{offIcon}</span>
-      </button>
+      <Button
+        className=" bg-background text-secondary-foreground hover:bg-secondary h-11 w-11 rounded-2xl font-semibold focus-visible:outline-black"
+        onClick={start}
+        disabled={disabled}
+      >
+        {offIcon}
+      </Button>
     )
 );
 
@@ -168,56 +199,9 @@ function ControlTray({
   return (
     <section className="control-tray">
       <canvas style={{ display: "none" }} ref={renderCanvasRef} />
-      <div className={cn("connection-container", { connected })}>
-        <div className="connection-button-container">
-          <button
-            ref={connectButtonRef}
-            className={cn("action-button connect-toggle", { connected })}
-            onClick={connected ? disconnect : connect}
-          >
-            <span className="material-symbols-outlined filled">
-              {connected ? "pause" : "play_arrow"}
-            </span>
-          </button>
-        </div>
-        <span className="text-indicator">Streaming</span>
-      </div>
-      <nav className={cn("actions-nav", { disabled: !connected })}>
-        <button
-          className={cn("action-button mic-button")}
-          onClick={() => setMuted(!muted)}
-        >
-          {!muted ? (
-            <span className="material-symbols-outlined filled">mic</span>
-          ) : (
-            <span className="material-symbols-outlined filled">mic_off</span>
-          )}
-        </button>
 
-        <div className="action-button no-action outlined">
-          <AudioPulse volume={volume} active={connected} hover={false} />
-        </div>
+      {children}
 
-        {supportsVideo && (
-          <>
-            <MediaStreamButton
-              isStreaming={screenCapture.isStreaming}
-              start={changeStreamr(screenCapture)}
-              stop={changeStreams()}
-              onIcon="cancel_presentation"
-              offIcon="present_to_all"
-            />
-            <MediaStreamButton
-              isStreaming={webcam.isStreaming}
-              start={changeStreams(webcam)}
-              stop={changeStreams()}
-              onIcon="videocam_off"
-              offIcon="videocam"
-            />
-          </>
-        )}
-        {children}
-      </nav>
       <PromptInput
         value={textInput}
         onValueChange={setTextInput}
@@ -226,7 +210,112 @@ function ControlTray({
         className="w-full max-w-(--breakpoint-md) text-md"
       >
         <PromptInputTextarea placeholder="Ask me anything..." />
-        <PromptInputActions className="justify-end pt-2">
+        <PromptInputActions className="flex items-center justify-between gap-2 pt-2">
+          <div className="flex items-center gap-x-1.5">
+            <PromptInputAction
+              delayDuration={0}
+              className="duration-0 data-[state=closed]:duration-0"
+              open={connected ? false : undefined}
+              tooltip={
+                !connected ? (
+                  <div className="bg-black">
+                    <Arrow className="fill-black" />
+                    <span className="text-xs leading-none font-semibold text-white">
+                      Voice mode
+                    </span>
+                  </div>
+                ) : undefined
+              }
+            >
+              <div
+                className={cn(
+                  "flex [&_svg]:shrink-0 items-center justify-center border rounded-full border-input ",
+                  { disabled: !connected },
+                  connected ? "pl-1" : "pl-0"
+                )}
+              >
+                <button
+                  ref={connectButtonRef}
+                  onClick={connected ? disconnect : connect}
+                  aria-label="Voice Mode"
+                  className="flex items-center justify-center  bg-background text-secondary-foreground hover:bg-secondary h-11 w-11 rounded-full font-semibold focus-visible:outline-black"
+                >
+                  {connected ? (
+                    <AudioPulse
+                      volume={volume}
+                      active={connected}
+                      hover={false}
+                    />
+                  ) : (
+                    <Waveform />
+                  )}
+                </button>
+                {connected && (
+                  <motion.div
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -20 }}
+                    transition={{ duration: 0.5, ease: "easeOut" }}
+                    className="flex gap-2 px-3"
+                  >
+                    <Button
+                      variant="ghost"
+                      className=" bg-background text-secondary-foreground hover:bg-secondary h-11 w-11 rounded-2xl font-semibold focus-visible:outline-black"
+                      onClick={() => setMuted(!muted)}
+                      disabled={!connected}
+                    >
+                      {!muted ? (
+                        <Mic className="size-5.5" />
+                      ) : (
+                        <MicOff className="size-5.5" />
+                      )}
+                    </Button>
+                    {supportsVideo && (
+                      <>
+                        <MediaStreamButton
+                          isStreaming={screenCapture.isStreaming}
+                          start={changeStreamr(screenCapture)}
+                          stop={changeStreams()}
+                          disabled={!connected}
+                          onIcon={<MonitorOff className="size-5.5" />}
+                          offIcon={<ScreenShare className="size-5.5" />}
+                        />
+                        <MediaStreamButton
+                          isStreaming={webcam.isStreaming}
+                          start={changeStreams(webcam)}
+                          stop={changeStreams()}
+                          disabled={!connected}
+                          onIcon={<Video className="size-5.5" />}
+                          offIcon={<VideoOff className="size-5.5" />}
+                        />
+                      </>
+                    )}
+                  </motion.div>
+                )}
+              </div>
+            </PromptInputAction>
+            <PromptInputAction
+              delayDuration={0}
+              className="duration-0 data-[state=closed]:duration-0"
+              tooltip={
+                <div className="bg-black">
+                  <Arrow className="fill-black" />
+                  <span className="text-xs leading-none font-semibold text-white">
+                    View tools
+                  </span>
+                </div>
+              }
+            >
+              <Button
+                variant="ghost"
+                size="icon"
+                aria-label="View tools"
+                className="border-input bg-background text-secondary-foreground hover:bg-secondary h-9 w-9 rounded-full border p-1 text-xs font-semibold focus-visible:outline-black [&_svg]:size-[18px]"
+              >
+                <Ellipsis className="size-6" />
+              </Button>
+            </PromptInputAction>
+          </div>
           <PromptInputAction
             tooltip={isLoading ? "Stop generation" : "Send message"}
           >
@@ -237,9 +326,9 @@ function ControlTray({
               onClick={handleSubmit}
             >
               {isLoading ? (
-                <Square className="size-6 fill-current" />
+                <Square className="size-5.5 fill-current" />
               ) : (
-                <ArrowUp className="size-6" />
+                <ArrowUp className="size-5.5" />
               )}
             </Button>
           </PromptInputAction>
